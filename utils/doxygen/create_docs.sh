024288b84b5361e51990ce4e46e5acd842f89966
James Bristow <james.bristow@plantandfood.co.nz>
2022-08-04 21:37:05 +1200

    Adding doxygen

--- utils/doxygen/create_docs.sh
+++ utils/doxygen/create_docs.sh
@@ -4,17 +4,25 @@
 # Main
 ###################################################################
 
-# [ -d docs ] && rm -rf docs 
+if [ ! -x "$(command -v doxygen)" ]; then
+  echo 'Error: doxygen is not installed.' >&2
+  exit 1
+fi
 
-# mkdir -p docs 
-# cp -r ../../* docs
-# cd docs
+if [ ! -x "$(command -v dot)" ]; then
+  echo 'Warning: Graphviz is not installed.' >&2
+fi
+ 
+[ -d docs ] && rm -rf docs 
 
-# find . -type f -name "*.rgg" -print0 | xargs -0 sed -i "s/module/class/g"
-# doxygen
+mkdir -p docs 
+cp -r ../../src/* docs
+cp Doxyfile docs
+cd docs
 
-# rm *.rgg
+find . -type f -name "*.rgg" -print0 | xargs -0 sed -i "s/module/class/g"
+doxygen
 
-# echo; echo "Open $(pwd)/html/index.html"
+echo; echo "Open $(pwd)/html/index.html"
 
-# cd ..
+cd ..
